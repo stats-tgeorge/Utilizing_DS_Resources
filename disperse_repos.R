deeec20056c19aff0c26a7146fdcf567b3dcf603
Tyler George <82473808+stats-tgeorge@users.noreply.github.com>
2021-11-16 08:16:44 -0600

    lab 2 updates

--- disperse_repos.R
+++ disperse_repos.R
@@ -3,7 +3,7 @@
 library(ghclass)
 library(gitcreds)
 
-gitcreds::gitcreds_set()
+#gitcreds::gitcreds_set()
 org_sitrep(org = "DSC223-FB4-2021")
 
 
@@ -28,16 +28,19 @@ org_pending('DSC223-FB4-2021')
 
 # First you need to create a repo out of just the hw/lab you want to disperse
 # Next make it a template
-repo_set_template('DSC223-FB4-2021/HW-1-pet-names')
 
-repo_is_template('DSC223-FB4-2021/HW-1-pet-names')
+repo_name = "AE-03-starwars-dataviz"
+
+repo_set_template(paste0("DSC223-FB4-2021/",repo_name))
+
+repo_is_template(paste0("DSC223-FB4-2021/",repo_name))
 
 # This will create an individual assingment
 org_create_assignment(
   org = "DSC223-FB4-2021",
   user = roster$github,
-  repo = paste0("hw-1-ind-", roster$github),
-  source_repo = 'DSC223-FB4-2021/HW-1-pet-names',
+  repo = paste0(paste0(repo_name,"-indi-"), roster$github),
+  source_repo = paste0("DSC223-FB4-2021/",repo_name),
   private = TRUE
 )
 
