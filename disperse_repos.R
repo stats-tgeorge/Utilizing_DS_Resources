7fcce6f5f134a2bdc619c04372db0df4f21dec3f
Tyler George <tgeorge@cornellcollege.edu>
2021-12-11 12:13:24 -0600

    shiny lab updates

--- disperse_repos.R
+++ disperse_repos.R
@@ -29,7 +29,7 @@ org_pending('DSC223-FB4-2021')
 # First you need to create a repo out of just the hw/lab you want to disperse
 # Next make it a template
 
-repo_name = "ae-09-feat-eng-cv"
+repo_name = "lab-15-shiny-intro"
 
 repo_set_template(paste0("DSC223-FB4-2021/",repo_name))
 
