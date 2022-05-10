6084756522716e472db67fe7ac5ea4c64f63be81
Tyler George <TGeorge@cornellcollege.edu>
2022-05-10 16:19:09 -0500

    Update convertnotestopdf.R

--- Notes/convertnotestopdf.R
+++ Notes/convertnotestopdf.R
@@ -4,6 +4,7 @@ setwd("U:/My Drive/Cornell College/Cornell Classes/Data Science/Notes/mod-u4-d09
 
 slide_name = "mod-u4-d09-CV"
 
+# This sometimes needs run twice
 build_pdf(paste0(slide_name,'.rmd'), complex_slides = TRUE, partial_slides = TRUE)
 #build_pdf(paste0(slide_name,'.html'), complex_slides = TRUE, partial_slides = TRUE)
 
